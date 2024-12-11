7b2b9fd75d240e6a356ad76530d6766547f25ce2
Brancovanbeek <branco.van.beek@hva.nl>
2024-12-11 09:38:37 +0100

    11-12

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ interaction.addEventListener('click', jumpHandler)
 // Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
 // _niet_ meteen bij het laden van de pagina aangeroepen
 function jumpHandler() {
-
   // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
   interaction.classList.toggle('jump')
 }
@@ -33,3 +32,79 @@ interaction.addEventListener('animationend', jumpHandler)
 
 // Stap 3: (Callback functie met) classList (.toggle(), .add(), etc.)
 // bibberLink.classList.toggle...
+
+//1
+let backgroundChange = document.querySelector('a:nth-of-type(1)')
+backgroundChange.addEventListener('click', backgroundColorChange)
+function backgroundColorChange() {
+  backgroundChange.classList.toggle('background-change')
+}
+
+//2
+let backgroundChangeTwee = document.querySelector('a:nth-of-type(2)')
+backgroundChangeTwee.addEventListener('click', backgroundColorChangeTwee)
+function backgroundColorChangeTwee() {
+  backgroundChangeTwee.classList.toggle('background-change-2')
+}
+
+
+//3
+let draaien = document.querySelector('a:nth-of-type(3)')
+draaien.addEventListener('click', rondjesDraaien)
+function rondjesDraaien(){
+  draaien.classList.toggle('rotate')
+}
+
+//4
+let trillen = document.querySelector('a:nth-of-type(4)')
+trillen.addEventListener('mouseover', opEnNeerTrillen)
+trillen.addEventListener('mouseout', stopTrillen);
+function opEnNeerTrillen() {
+  trillen.classList.toggle('shaking')
+}
+function stopTrillen() {
+  trillen.classList.remove('shaking');
+}
+
+//5
+let horizontaalTrillen = document.querySelector('a:nth-of-type(5)')
+horizontaalTrillen.addEventListener('mouseover', horizontaalTrillenFunctie)
+horizontaalTrillen.addEventListener('mouseout', stopHorizontaalTrillen);
+function horizontaalTrillenFunctie() {
+  horizontaalTrillen.classList.toggle('horizontal-shaking')
+}
+function stopHorizontaalTrillen() {
+  horizontaalTrillen.classList.remove('horizontal-shaking');
+}
+
+//6
+let keyButton = document.querySelector('a:nth-of-type(6)')
+document.addEventListener('keydown', toetsIngedrukt)
+function toetsIngedrukt(event){
+  if(event.code == 'Enter'){
+    keyButton.classList.toggle('color-change')
+  }
+}
+
+//7
+let spaceRotate = document.querySelector('a:nth-of-type(7)')
+document.addEventListener('keydown', spaceIndrukken)
+function spaceIndrukken(event){
+  if(event.code == 'Space'){
+    spaceRotate.classList.toggle('space-rotate')
+  }
+}
+
+//8
+let scale = document.querySelector('a:nth-of-type(8)')
+scale.addEventListener('click', schalen)
+function schalen(){
+  scale.classList.toggle('scale')
+}
+
+//9
+let scaleSmaller = document.querySelector('a:nth-of-type(9)')
+scaleSmaller.addEventListener('click', kleinerSchalen)
+function kleinerSchalen(){
+  scaleSmaller.classList.toggle('scale-smaller')
+}
